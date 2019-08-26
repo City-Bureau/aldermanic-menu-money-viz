84f935963970a6d389ea7dc4938f705454483b80
pjsier <pjsier@gmail.com>
2019-08-26 11:38:10 -0500

    Update deploy script

--- .deploy.sh
+++ .deploy.sh
@@ -4,5 +4,5 @@ rm -f dist/*
 npm run build
 # Gzip encode files, then remove extension for upload
 for f in dist/*.{map,js,css,csv,geojson}; do gzip -9 $f && mv $f.gz $f; done
-aws s3 cp dist/ s3://city-bureau-demos/menu-money/ --exclude "*.html" --recursive --content-encoding=gzip --acl=public-read
-aws s3 cp dist/index.html s3://city-bureau-demos/menu-money/ --acl=public-read
+aws s3 cp dist/ s3://city-bureau-graphics/menu-money/ --exclude "*.html" --recursive --content-encoding=gzip --acl=public-read
+aws s3 cp dist/index.html s3://city-bureau-graphics/menu-money/ --acl=public-read
